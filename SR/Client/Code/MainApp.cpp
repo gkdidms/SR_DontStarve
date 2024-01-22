65e89ca6c55996f4d89772b6f1acd02b5baeeecd
Parksewon86 <sewonb86@gmail.com>
2024-01-22 20:56:08 +0900

    Test Commit

--- SR/Client/Code/MainApp.cpp
+++ SR/Client/Code/MainApp.cpp
@@ -15,8 +15,9 @@ HRESULT CMainApp::Ready_MainApp()
 {
 	FAILED_CHECK_RETURN(SetUp_Setting(&m_pGraphicDev), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Scene(m_pGraphicDev, &m_pManagementClass), E_FAIL);
-	
+	//adfasf
 	//teests
+	//sss
 	return S_OK;
 }
 
