07cd74a8f71ed486a7f1a654049b4c0554734415
Parksewon86 <sewonb86@gmail.com>
2024-01-22 20:23:10 +0900

    test

--- SR/Client/Code/MainApp.cpp
+++ SR/Client/Code/MainApp.cpp
@@ -16,6 +16,7 @@ HRESULT CMainApp::Ready_MainApp()
 	FAILED_CHECK_RETURN(SetUp_Setting(&m_pGraphicDev), E_FAIL);
 	FAILED_CHECK_RETURN(Ready_Scene(m_pGraphicDev, &m_pManagementClass), E_FAIL);
 	
+	//teests
 	return S_OK;
 }
 
